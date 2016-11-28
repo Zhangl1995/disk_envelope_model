f0adb9e447fb9cab176e42612a71c2c239631736
Carolina Agurto Gangas <cagurto@mpe.mpg.de>
2016-11-28 14:21:35 +0100

    Update setup_model.py

--- setup_model.py
+++ setup_model.py
@@ -16,15 +16,32 @@ radmc3dPy.setup.problemSetupDust('carolina',binary=True)
 
 # Calculate the dust temperature
 os.system('radmc3d mctherm setthreads 4')
-os.system('radmc3d sed incl 67 phi 30 setthreads 40')
+os.system('radmc3d sed incl 67 phi 30 setthreads 4')
+
+#Making SED
+#in terminal xman
+radmc3d spectrum incl 45 phi 30 lambdarange 1 1500 nlam 100
+radmc3d spectrum incl 45 phi 30 lambdarange 10 3000 nlam 100
+radmc3d spectrum incl 45 phi 30 lambdarange 10 3000 nlam 1000
+
+
+#in IDL
+.r readradmc
+s=readspectrum()
+set_plot,'ps'
+device,filename='spectrum_avthin5_45_phi30_lambdarange_1_1500_100.ps'
+plotspectrum,s,dpc=240.,/jy
+device,/close_file
+
 
 #Make an image
-radmc3dPy.image.makeImage(npix=1000,sizeau=2000,wav=2775.,incl=67,posang=170.)
+radmc3dPy.image.makeImage(npix=1000,sizeau=100.,wav=3000.,incl=67,posang=80)
 imag=radmc3dPy.image.readImage()
-radmc3dPy.image.plotImage(imag,au=True,dpc=240.,log=True,maxlog=5,cmap='hot')
-plb.savefig('figure+e6pho.eps', bbox_inches='tight')
+#radmc3dPy.image.plotImage(imag,au=True,dpc=240.,log=True,maxlog=5,cmap='afmhot')
+radmc3dPy.image.plotImage(imag,au=True,dpc=240.,log=True,maxlog=5,cmap='CMRmap')
+#plb.savefig('figure+e6pho.eps', bbox_inches='tight')
 #plb.savefig("/Users/cagurto/Documents/Newradmc3d/version_0.39/python/model-py/figure1.png")
-#plb.clf()
+plb.clf()
 
 # 2D TEMPERATURE CONTOUR (natconst.au=1.496e13)
 data = radmc3dPy.analyze.readData(dtemp=True)
@@ -36,7 +53,7 @@ cb = plb.colorbar(c)
 cb.set_label('T [K]', rotation=270.)
 c = plb.contour(data.grid.x/natconst.au, np.pi/2.-data.grid.y, data.dusttemp[:,:,0,0].T, 10, colors='k',linestyles='solid')
 plb.clabel(c, inline=1, fontsize=10)
-plb.savefig('figure2Dtempcontour.eps', bbox_inches='tight')
+#plb.savefig('figure2Dtempcontour.eps', bbox_inches='tight')
 plb.show()
 
 # Dust density contours
@@ -48,7 +65,7 @@ plb.ylabel(r'$\pi/2-\theta$')
 plb.xscale('log')
 cb = plb.colorbar(c2)
 cb.set_label(r'$\log_{10}{\rho}$', rotation=270.)
-plb.savefig('figure-dustdesnity.eps', bbox_inches='tight')
+#plb.savefig('figure-dustdesnity.eps', bbox_inches='tight')
 plb.show()
 
 # dust opacity
